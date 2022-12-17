df0d78b2ddb20e51f1c2aa5ca60c87f0641dfc65
Roman Baranov <roma.baranov@gmail.com>
2022-12-17 12:16:53 +0300

    Add Book and Author models

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,19 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    abstract = models.TextField(blank=True)
+    amount = models.IntegerField(
+        default=0,
+        validators=[
+            MinValueValidator(limit_value=0),
+        ],
+    )
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    about = models.TextField(blank=True)
+    books = models.ManyToManyField(Book)
