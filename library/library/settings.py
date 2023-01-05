2b6b6bbf94cd91c927222e555fbefb53a7925529
Roman Baranov <roma.baranov@gmail.com>
2023-01-05 11:29:49 +0300

    Navbar reorganization

--- library/library/settings.py
+++ library/library/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/',
+]
 STATIC_URL = 'static/'
 
 # Default primary key field type
