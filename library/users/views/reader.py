be98f3d8779cbd51c2a648edef3c3933a7198228
Roman Baranov <roma.baranov@gmail.com>
2023-01-05 17:15:26 +0300

    Change reader views

--- library/users/views/reader.py
+++ library/users/views/reader.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
-from django.views.generic.edit import FormView
+from django.views.generic import (
+    FormView,
+    TemplateView,
+)
 
 from users.forms import ReaderRegistrationForm
 from users.models import Reader
@@ -53,9 +56,10 @@ class ReaderLogoutView(LogoutView):
 class ReaderProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'reader/profile.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user = self.request.user
+    def get(self, request, *args, **kwargs):
+        user = request.user
         reader = Reader.objects.filter(user__id=user.id).first()
-        context['reader'] = reader
-        return context
+        if reader is not None:
+            return super().get(request, *args, **kwargs)
+        else:
+            return redirect('/')
