ce4482a511d12a2b1474f434887e9baab10d1d22
Roman Baranov <roma.baranov@gmail.com>
2023-01-05 10:24:39 +0300

    Change 'books' application urls

--- library/library/urls.py
+++ library/library/urls.py
@@ -22,22 +22,22 @@ from users import views as user_views
 urlpatterns = [
     path('', book_views.welcome, name='welcome'),
 
-    path('genre/', book_views.genre.GenreListView.as_view(), name='genre-list'),
-    path('genre/<int:pk>/', book_views.genre.GenreDetailView.as_view(), name='genre-details'),
+    path('genres/', book_views.genre.GenreListView.as_view(), name='genre-list'),
+    path('genres/<int:pk>/', book_views.genre.GenreDetailView.as_view(), name='genre-details'),
 
-    path('work/', book_views.work.WrittenWorkListView.as_view(), name='work-list'),
-    path('work/<int:pk>/', book_views.work.WrittenWorkDetailView.as_view(), name='work-details'),
+    path('works/', book_views.work.WrittenWorkListView.as_view(), name='work-list'),
+    path('works/<int:pk>/', book_views.work.WrittenWorkDetailView.as_view(), name='work-details'),
 
-    path('author/', book_views.author.AuthorListView.as_view(), name='author-list'),
-    path('author/<int:pk>/', book_views.author.AuthorDetailView.as_view(), name='author-details'),
+    path('authors/', book_views.author.AuthorListView.as_view(), name='author-list'),
+    path('authors/<int:pk>/', book_views.author.AuthorDetailView.as_view(), name='author-details'),
 
-    path('book/', book_views.book.BookListView.as_view(), name='book-list'),
-    path('book/<int:pk>/', book_views.book.BookDetailView.as_view(), name='book-details'),
-    path('book/<int:pk>/get/', book_views.book.GetBookView.as_view(), name='book-get'),
-    path('book/<int:pk>/return/', book_views.book.ReturnBookView.as_view(), name='book-return'),
+    path('books/', book_views.book.BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', book_views.book.BookDetailView.as_view(), name='book-details'),
+    path('books/<int:pk>/get/', book_views.book.GetBookView.as_view(), name='book-get'),
+    path('books/<int:pk>/return/', book_views.book.ReturnBookView.as_view(), name='book-return'),
 
-    path('publisher/', book_views.publisher.PublisherListView.as_view(), name='publisher-list'),
-    path('publisher/<int:pk>/', book_views.publisher.PublisherDetailView.as_view(), name='publisher-details'),
+    path('publishers/', book_views.publisher.PublisherListView.as_view(), name='publisher-list'),
+    path('publishers/<int:pk>/', book_views.publisher.PublisherDetailView.as_view(), name='publisher-details'),
 
     path('users/registration/', user_views.reader.ReaderCreateView.as_view(), name='user-registration'),
     path('users/login/', user_views.reader.ReaderLoginView.as_view(), name='user-login'),
