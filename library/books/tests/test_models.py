55febf68a8e21a25baf28cf75ede4da90613c69c
Roman Baranov <roma.baranov@gmail.com>
2023-01-27 19:58:01 +0300

    WrittenWork model tests

--- library/books/tests/test_models.py
+++ library/books/tests/test_models.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from faker import Faker
+from mixer.backend.django import mixer
 
 from books.models import (
     Genre,
+    WrittenWork,
 )
 
 
@@ -26,4 +28,28 @@ class TestGenreModel(TestCase):
         genres = Genre.objects.all()
         genre_names = [g.name for g in genres]
         sorted_genre_names = sorted(genre_names)
-        self.assertListEqual(sorted_genre_names, genre_names)
+        self.assertListEqual(genre_names, sorted_genre_names)
+
+
+class TestWrittenWorkModel(TestCase):
+
+    def setUp(self) -> None:
+        num_of_works = 13
+        faker = Faker()
+        titles = faker.words(nb=num_of_works, unique=True)
+        mixer.cycle(num_of_works).blend(WrittenWork, title=(t for t in titles))
+
+    def tearDown(self) -> None:
+        WrittenWork.objects.all().delete()
+        Genre.objects.all().delete()
+
+    def test_str(self):
+        works = WrittenWork.objects.all()
+        for work in works:
+            self.assertEqual(str(work), work.title)
+
+    def test_ordering(self):
+        works = WrittenWork.objects.all()
+        titles = [work.title for work in works]
+        sorted_titles = sorted(titles)
+        self.assertListEqual(titles, sorted_titles)
